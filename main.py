e2708bdd59f1888b657cd063294d5afee060ada1
iankitsubedi <ankitsubedi98765@gmail.com>
2025-07-22 13:27:12 +0545

    updated bugs

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+num = []
 a = int(input("Enter a number in decimal number system: "))
+
+while a > 0:
+    x = a % 2 
+    num.append(x)
+    a = a // 2
+
+num.reverse()
+print("The number in binary system is :")
+for i in num:
+    print(i,end = ' ')
